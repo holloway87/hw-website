56de01bee30baf7964b284ae2f0af24d2998ea33
holloway <me@holloway-web.de>
2022-02-04 09:28:50 +0100

    added the website to the programming section

--- assets/views/dev.vue
+++ assets/views/dev.vue
@@ -2,10 +2,29 @@
     <div>
         <h1>Programming</h1>
 
-        <p>Nothing to see here yet.</p>
+        <p>These are some of my programming projects.</p>
+
+        <b-row>
+            <b-col sm="6" md="4">
+                <b-card title="My website">
+                    <b-card-text>The code to this website can be found on my github.</b-card-text>
+
+                    <b-button
+                        href="https://github.com/holloway87/hw-website"
+                        rel="nofollow"
+                        target="_blank"
+                        variant="primary"
+                    ><b-icon-github /> GitHub source</b-button>
+                </b-card>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
-export default {};
+import { BIconGithub } from 'bootstrap-vue';
+
+export default {
+    components: { BIconGithub },
+};
 </script>

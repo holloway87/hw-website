ceb9a99387affb1808c06922f4bae7f3a19f3473
holloway <me@holloway-web.de>
2022-05-19 13:08:14 +0200

    added social media links

--- assets/App.vue
+++ assets/App.vue
@@ -1,9 +1,19 @@
 <template>
-    <router-view />
+    <div>
+        <router-view />
+        <div class="text-center">
+            <a rel="me" href="https://mastodon.art/@holloway" target="_blank"><b-icon-mastodon /></a> &middot;
+            <a rel="nofollow" href="https://www.instagram.com/me_hw/" target="_blank"><b-icon-instagram /></a> &middot;
+            <a rel="nofollow" href="https://twitter.com/me_hw" target="_blank"><b-icon-twitter /></a>
+        </div>
+    </div>
 </template>
 
 <script>
+import { BIconInstagram, BIconMastodon, BIconTwitter } from 'bootstrap-vue';
+
 export default {
+    components: { BIconInstagram, BIconMastodon, BIconTwitter },
     data() {
         return {};
     },

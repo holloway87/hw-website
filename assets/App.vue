2dcdb5006268c42a33d38307d4f0a6e3dd1ed620
holloway <me@holloway-web.de>
2024-02-28 20:18:38 +0100

    changed top bar height, color and shadow

--- assets/App.vue
+++ assets/App.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="app">
-        <div class="sticky top-0 left-0 right-0 bg-[#1f2e2b] border-b border-[#151f1d] shadow-md z-20 mb-5">
+        <div class="sticky top-0 left-0 right-0 bg-[#151f1d] border-b border-black shadow-md shadow-gray-900/65 h-9 z-20 mb-5">
             <div class="container mx-auto lg:max-w-[1140px]">
-                <div class="flex flex-row place-content-stretch">
-                    <router-link to="/" class="inline-block text-white text-xl ml-4">hw-web</router-link>
+                <div class="flex flex-row items-center">
+                    <router-link to="/" class="inline-block text-white text-xl leading-9 ml-4">hw-web</router-link>
 
                     <div v-if="store.back_url" class="grow text-right">
                         <router-link
-                            class="inline-block h-7 bg-teal-600 text-white text-sm text-center leading-5 pl-1.5 pr-4 sm:pr-2.5 sm:mr-4 py-1 align-middle"
+                            class="inline-block h-9 bg-teal-600 border-b border-black text-white text-md text-center leading-7 pl-1.5 pr-4 sm:pr-2.5 sm:mr-4 py-1 align-middle"
                             :to="store.back_url"
                         >
-                            <ChevronLeftIcon class="h-4 inline-block align-sub" />
+                            <ChevronLeftIcon class="h-5 inline-block align-text-bottom" />
                             back
                         </router-link>
                     </div>

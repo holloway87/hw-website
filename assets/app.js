917ceb427355a69977a41d24ca101769e39d6bd8
holloway <me@holloway-web.de>
2024-03-12 11:20:38 +0100

    renamed App.vue to Main.vue due to case problems on Mac

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,12 @@ import { createPinia } from 'pinia';
 
 import router from './router';
 
-import App from './App';
+import Main from './Main';
 import './styles/app.css';
 
 const pinia = createPinia();
 
-const app = createApp(App);
+const app = createApp(Main);
 app.use(pinia);
 app.use(router);
 app.mount('#app');

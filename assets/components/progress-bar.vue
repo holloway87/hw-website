180359eee98129494cc06dfe63acc91a3c685443
holloway <me@holloway-web.de>
2024-05-03 16:06:58 +0200

    added progress bar component

---
+++ assets/components/progress-bar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="border border-[#1f2e2b] rounded-md h-6 overflow-hidden mb-1">
+        <div class="bg-white h-full" :style={width:progress}></div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    'progress': {
+        'required': true,
+        'type': String
+    }
+});
+</script>

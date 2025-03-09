3b0ba70038815d2226d1aaaebb76ab62e32f79ad
holloway <me@holloway-web.de>
2025-03-09 19:21:58 +0100

    Added sizes lg, 2xl, 3xl and 4xl to modal vue component

--- assets/components/modal.vue
+++ assets/components/modal.vue
@@ -2,7 +2,21 @@
     <div v-if="show" class="fixed inset-0 overflow-auto z-40">
         <div class="fixed inset-0 bg-[#1f2e2b] bg-opacity-85"></div>
         <div class="flex min-h-full items-center justify-center px-4 py-8">
-            <div class="bg-[#151f1d] rounded-md shadow-md shadow-gray-900/65 text-white w-full max-w-lg z-40">
+            <div
+                :class="{
+                    'bg-[#151f1d]': true,
+                    'rounded-md': true,
+                    'shadow-md': true,
+                    'shadow-gray-900/65': true,
+                    'text-white': true,
+                    'w-full': true,
+                    'max-w-lg': 'lg' === size,
+                    'max-w-2xl': '2xl' === size,
+                    'max-w-3xl': '3xl' === size,
+                    'max-w-4xl': '4xl' === size,
+                    'z-40': true
+                }"
+            >
                 <div class="px-4 pt-2 pb-1">
                     <slot name="header">
                         <h3 class="text-2xl">{{ title }}</h3>
@@ -28,6 +42,11 @@ defineProps({
     'title': {
         'required': false,
         'type': String,
-    }
+    },
+    'size': {
+        'default': 'lg',
+        'required': false,
+        'type': String,
+    },
 });
 </script>

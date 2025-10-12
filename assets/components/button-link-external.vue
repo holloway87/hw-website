832cf01af3dfe443e4c5f8e9ed1d91f829c0af83
holloway <me@holloway-web.de>
2025-10-12 13:27:15 +0200

    Added border design, hover and click effect on buttons

--- assets/components/button-link-external.vue
+++ assets/components/button-link-external.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :href="url" class="inline-block bg-teal-600 text-white rounded-md text-center px-3 py-1.5">
+    <a :href="url" class="inline-block bg-teal-600 border-b-4 border-b-teal-700 hover:border-t-1 hover:border-t-teal-600 hover:border-b-3 active:border-t-1 active:border-t-teal-600 active:border-b-3 text-white rounded-md text-center px-3 py-1.5">
         <slot />
     </a>
 </template>
